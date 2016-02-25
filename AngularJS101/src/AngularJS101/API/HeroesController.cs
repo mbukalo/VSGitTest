e1f2fd2c5e34a7d97442d7d47105e9b26b31b37c
mbukalo <mbukalo@gmail.com>
2016-02-24 22:14:08 -0500

    first commit

---
+++ AngularJS101/src/AngularJS101/API/HeroesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using AngularJS101.Models;
+
+namespace AngularJS101.API
+{
+    [Route("api/[controller]")]
+    public class HeroesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<DOTAHero> Get()
+        {
+            HeroManager HM = new HeroManager();
+            return HM.GetAll;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public DOTAHero Get(int id)
+        {
+            HeroManager HM = new HeroManager();
+            return HM.GetHeroByID(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
